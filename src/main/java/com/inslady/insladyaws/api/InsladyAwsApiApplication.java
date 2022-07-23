224c44e70f3f5b7890e789e26b782a260b0985fd
TerryChen <chocoterry@163.com>
2022-07-23 21:09:04 +0900

    init

---
+++ src/main/java/com/inslady/insladyaws/api/InsladyAwsApiApplication.java
@@ -0,0 +1,13 @@
+package com.inslady.insladyaws.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsladyAwsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InsladyAwsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inslady/insladyaws/InsladyAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inslady.insladyaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsladyAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
